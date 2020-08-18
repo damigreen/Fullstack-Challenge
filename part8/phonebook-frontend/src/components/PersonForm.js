cd7ac49d11260c2be650e2e1b14448917867dd30
Damilola Faseun <fashfired@gmail.com>
2020-08-18 13:47:06 +0100

    Added a form to add a new person

---
+++ part8/phonebook-frontend/src/components/PersonForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+
+const PersonForm = (props) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+
+  const submit = async (e) => {
+    e.preventDefault();
+    await props.addPerson({
+      variables: { name, phone, street, city }
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit} >
+        <div>
+          <label for="Name">Name</label>
+          <input
+            value={name}
+            onChange={ ({target}) => setName(target.value)}
+          />
+        </div>
+        <div>
+          <label for="phone">Phone</label>
+          <input
+            value={phone}
+            onChange={ ({target}) => setPhone(target.value)}
+          />
+        </div>
+        <div>
+          <label for="street">Street</label>
+          <input
+            value={street}
+            onChange={ ({target}) => setStreet(target.value)}
+          />
+        </div>
+        <div>
+          <label for="city">City</label>
+          <input
+            value={city}
+            onChange={ ({target}) => setCity(target.value)}
+          />
+        </div>
+        <button type="submit">add!</button>
+      </form>
+    </div>
+  )
+}
+
+export default PersonForm
