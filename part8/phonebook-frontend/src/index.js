f232889504a34c73a8bd894477892affb0b6e167
Damilola Faseun <fashfired@gmail.com>
2020-08-20 19:47:39 +0100

    Configure apollo client to use hooks

--- part8/phonebook-frontend/src/index.js
+++ part8/phonebook-frontend/src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from 'react-apollo'
-import { client } from './App';
+// import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks'
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+
+
 
 ReactDOM.render(
   <React.StrictMode>
