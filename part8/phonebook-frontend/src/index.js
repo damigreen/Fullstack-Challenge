3e1a667c872984b12fb2ff712bd27b514cc76c0c
Damilola Faseun <fashfired@gmail.com>
2020-08-28 11:14:22 +0100

    Use the apollo-boost client now

--- part8/phonebook-frontend/src/index.js
+++ part8/phonebook-frontend/src/index.js
@@ -2,37 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 // import { ApolloProvider } from 'react-apollo'
-// import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks'
-import { ApolloClient } from 'apollo-client'
+// import { ApolloClient } from 'apollo-client'
 import {createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
+// import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 
-
-// const client = new ApolloClient({
-//   uri: 'http://localhost:4000'
-// })
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql'
-})
-
-const authLink = setContext((_, { headers}) => {
-  const token = localStorage.getItem('phonenumbers-user-token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `bearer ${token}` : null
-    }
-  }
-})
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  uri: 'http://localhost:4000'
 })
+
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:4000/graphql'
+// })
+
+
+// // Adding token from localstorage
+// // and set as the Authorization headers
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('phonenumbers-user-token');
+//   console.log(token)
+
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `bearer ${token}` : null
+//     }
+//   }
+// })
+
+// // ApolloConstructure function using the parameters
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// })
 
 ReactDOM.render(
   <React.StrictMode>
@@ -42,8 +50,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
