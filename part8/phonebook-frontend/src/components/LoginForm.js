04a96b3514c473a91893524c0f13562f11162557
Damilola Faseun <fashfired@gmail.com>
2020-08-28 07:13:29 +0100

    Hooked up the login form

--- part8/phonebook-frontend/src/components/LoginForm.js
+++ part8/phonebook-frontend/src/components/LoginForm.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function ({login, setToken}) {
+function LoginForm(props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submit = async (e) => {
+    e.preventDefault()
+    
+    const result = await props.login({
+      variables: { username, password }
+    });
+
+    if (result) {
+      const token = result.data.login;
+      props.setToken(token);
+      localStorage.setItem('phonenumbers-user-token', token);
+    }
+  }
+
   return (
     <div>
-      
+      <form onSubmit={submit}>
+        <div>
+          <label for="username">Username</label>
+          <input
+            value={username}
+            onChange={({target}) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </div>
   )
 }
