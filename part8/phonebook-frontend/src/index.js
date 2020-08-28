11ac46388e0cc7295fc92b1c7ff3bf5dda5a6131
Damilola Faseun <fashfired@gmail.com>
2020-08-28 08:34:28 +0100

    Define the client using the ApolloClient of apollo-link

--- part8/phonebook-frontend/src/index.js
+++ part8/phonebook-frontend/src/index.js
@@ -4,15 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import { ApolloProvider } from 'react-apollo'
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from '@apollo/react-hooks'
+// import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import {createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000'
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000'
+// })
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
 })
 
+const authLink = setContext((_, { headers}) => {
+  const token = localStorage.getItem('phonenumbers-user-token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null
+    }
+  }
+})
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
