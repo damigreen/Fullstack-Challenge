2cb609e3b069c0810a365652363381844b4904fb
Damilola Faseun <fashfired@gmail.com>
2020-11-02 22:11:24 +0100

    Added a services to get all entries (diaries)

---
+++ part9/zord/src/services/diaryServices.ts
@@ -0,0 +1,18 @@
+import diaryData from '../../data/diaries.json';
+import { DiaryEntry } from '../types';
+
+const diaries:  Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+
+
+const getEntities = (): Array<DiaryEntry> => {
+  return diaries;
+};
+
+const addEntry = () => {
+  return null;
+};
+
+export default {
+  getEntities,
+  addEntry
+};
