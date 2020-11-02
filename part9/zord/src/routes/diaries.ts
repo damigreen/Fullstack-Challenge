428a3fd28563973edd31ec1c79134c669449c18f
Damilola Faseun <fashfired@gmail.com>
2020-11-02 22:57:06 +0100

    route  to  get all diary logic

--- part9/zord/src/routes/diaries.ts
+++ part9/zord/src/routes/diaries.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import diaryServices from '../services/diaryServices';
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  res.send('Getting all diaries');
-});
+// router.get('/', (_req, res) => {
+//   res.send('Getting all diaries');
+// });
 
 router.post('/', (_req, res) => {
   res.send('Saving a diary');
+});
+
+
+router.get('/', (_req, res) => {
+  res.send(diaryServices.getNonSensitiveSntries());
 });
 
 
