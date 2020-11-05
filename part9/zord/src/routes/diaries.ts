70fc9575eeb791ce08dcaa9a64c6a6ce5ebf64f9
Damilola Faseun <fashfired@gmail.com>
2020-11-05 21:19:44 +0100

    Route to get and post a diary

--- part9/zord/src/routes/diaries.ts
+++ part9/zord/src/routes/diaries.ts
@@ -7,14 +7,33 @@ const router = express.Router();
 //   res.send('Getting all diaries');
 // });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary');
-});
+// router.post('/', (_req, res) => {
+//   res.send('Saving a diary');
+// });
 
 
 router.get('/', (_req, res) => {
   res.send(diaryServices.getNonSensitiveEntries());
 });
 
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(Number(req.params.id));
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body;
+  const newDiaryEntry = diaryServices.addDiary(
+    date,
+    weather,
+    visibility,
+    comment,
+  );
+  res.json(newDiaryEntry);
+});
 
 export default router;
