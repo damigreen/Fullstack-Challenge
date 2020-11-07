387e571c2c31a67d054d9811778835f87448f40c
Damilola Faseun <fashfired@gmail.com>
2020-11-07 08:05:29 +0100

    Defined visibility using enum

--- part9/zord/src/types.ts
+++ part9/zord/src/types.ts
@@ -6,7 +6,13 @@ export enum Weather {
   Windy = 'windy',
   Stormy = 'stormy'
 }
-export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+// export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor'
+}
 
 export interface DiaryEntry {
   id: number;
