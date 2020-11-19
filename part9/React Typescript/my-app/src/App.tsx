4082852f221d010a56a2fe3838afa6c1e1bfb69f
Damilola Faseun <fashfired@gmail.com>
2020-11-19 10:20:03 +0100

    Define a type for the welcome component props

--- part9/React Typescript/my-app/src/App.tsx
+++ part9/React Typescript/my-app/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome: React.FC<WelcomeProps> = (props) => {
+  return <h1>Hello, {props.name}</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome name="Sandra" />
     </div>
   );
 }
+
+
+// const element = <Welcome name="Sara" />;
+// ReactDOM.render(element, document.getElementById("root"));
 
 export default App;
