f2fdeb8fde78d46a64fc1361975ea7979c22e996
Damilola Faseun <fashfired@gmail.com>
2020-11-19 10:48:43 +0100

    Less verbose syntax for type definition

--- part9/React Typescript/my-app/src/App.tsx
+++ part9/React Typescript/my-app/src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-interface WelcomeProps {
-  name: string;
-}
+// interface WelcomeProps {
+//   name: string;
+// }
 
-const Welcome: React.FC<WelcomeProps> = (props) => {
-  return <h1>Hello, {props.name}</h1>;
-};
+// const Welcome: React.FC<WelcomeProps> = (props) => {
+//   return <h1>Hello, {props.name}</h1>;
+// };
+
+const Welcome: React.FC<{ name: string }> = ({ name }) => (
+  <h1>Hello, {name}</h1>
+);
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Sandra" />
+      <Welcome name="Damigreen" />
     </div>
   );
 }
-
 
 // const element = <Welcome name="Sara" />;
 // ReactDOM.render(element, document.getElementById("root"));
